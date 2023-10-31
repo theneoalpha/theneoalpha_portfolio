e8f9328bb7913eb4212e9c23f97fe0aa9e95bb8f
Vikash <vikashkaushik910@gmail.com>
2023-10-31 11:22:06 +0530

    Version 2

---
+++ client/src/components/Github.js
@@ -0,0 +1,23 @@
+import React from "react";
+import GitHubCalendar from "react-github-calendar";
+import { Row } from "react-bootstrap";
+
+
+export default function Github() {
+  return (
+    <Row style={{ justifyContent: "center", padding: "4vh 4.5vh" ,marginTop:"4vh"  }}>
+      <h1 className="project-heading " style={{ paddingBottom: "20px", margin: "auto",width:"40vh" }}>
+        Days I <strong className="purple">Code</strong>
+      </h1>
+      <GitHubCalendar
+        username="theneoalpha"
+        blockSize={15}
+        blockMargin={5}
+        color="#c084f5"
+        fontSize={16}
+      />
+    </Row>
+  );
+}
+
+
