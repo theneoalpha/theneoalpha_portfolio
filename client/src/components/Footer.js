017657247c994c941f6fcb35a7f050feae9f6ad9
Vikash <vikashkaushik910@gmail.com>
2023-11-08 16:09:06 +0530

    Build file updated after project section

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,10 @@
-import React from 'react'
+import React from 'react';
+// import InstagramIcon from '@mui/icons-material/Instagram';
+// import GitHubIcon from '@mui/icons-material/GitHub';
+// import LinkedInIcon from '@mui/icons-material/LinkedIn';
+// import YouTubeIcon from '@mui/icons-material/YouTube';
 
+import "../components/assets/footer.css";
 export default function Footer(props) {
 
   return (
@@ -7,7 +12,17 @@ export default function Footer(props) {
     <div id="footer">
 
     <p>
-Copyright &copy; 2023   | All Right Reserved   </p>         
+Copyright &copy; 2023   | All Right Reserved  
+
+
+
+ </p>         
+ {/* <li className="footerSocial"> <a href="https://www.instagram.com/theneoalpha/" ><InstagramIcon/></a>
+        <a href="https://github.com/theneoalpha" ><GitHubIcon/></a>
+        <a href="https://www.linkedin.com/in/theneoalpha/" ><LinkedInIcon/></a>
+        <a href="https://www.youtube.com/c/TheNeoAlpha"><YouTubeIcon/></a>
+       
+        </li> */}
 
     </div>
     
