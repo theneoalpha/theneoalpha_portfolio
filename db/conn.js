b69e0cb25cfb221723debded382b133fc8c67c88
Vikash Kaushik <103592069+theneoalpha@users.noreply.github.com>
2024-01-16 01:47:21 +0530

    Reverting to old database

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const DB ="mongodb+srv://theneoalpha:VikashKaushik@cluster0.nkno9.mongodb.net/?retryWrites=true&w=majority"
+const DB ="mongodb+srv://theneoalpha:VikashKaushik@cluster0.av3i3bq.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(DB).then(()=>{
     console.log("Connection successul");
 }).catch((err)=>{
