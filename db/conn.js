909715daf6e10caee4ce0dba7a61441fe7692b67
Vikash Kaushik <103592069+theneoalpha@users.noreply.github.com>
2024-06-11 09:01:24 +0530

    Dele

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const DB ="mongodb+srv://theneoalpha:VikashKaushik@cluster0.nkno9.mongodb.net/?retryWrites=true&w=majority"
+const DB ="mongodb+srv://theneoalpha1:VikashKaushik@cluster0.pydiecm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 mongoose.connect(DB).then(()=>{
     console.log("Connection successul");
 }).catch((err)=>{
